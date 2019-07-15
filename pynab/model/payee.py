b7be90f990af7b841fd6b26b3c40b7ca440a2564
Dennis Whitney <dennis@irunasroot.com>
2019-07-14 20:59:32 -0500

    added payee

---
+++ pynab/model/payee.py
@@ -0,0 +1,34 @@
+"""
+payee.py - desc
+Comments: 
+Author: Dennis Whitney
+Email: dennis@runasroot.com
+Copyright © 2019, iRunAsRoot 
+"""
+
+from . import JsonDict, JsonList
+
+
+class Payee(JsonDict):
+    """
+    Single payee object
+    """
+
+    fields = {
+        "id": str(),
+        "name": str(),
+        "transfer_account_id": str(),
+        "deleted": bool()
+    }
+
+    def __init__(self):
+        super().__init__()
+
+
+class Payees(JsonList):
+    """
+    An object that includes a list of single payee objects
+    """
+
+    def __init__(self):
+        super().__init__()
