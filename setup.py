4c42ccdeda0d189448cc99458c8ee753813f80ce
Dennis Whitney <dennis@irunasroot.com>
2019-07-17 18:29:58 -0500

    Changing packaging name due to conflict

--- setup.py
+++ setup.py
@@ -7,24 +7,24 @@ Copyright (c) 2019, iRunAsRoot
 
 from distutils.core import setup
 setup(
-  name = 'pynab',
-  packages = ['pynab'],
-  version = '0.1',
-  license='GNU GPLv3',
-  description = 'Python API to access YNAB v1 API',
-  author = 'Dennis Whitney',
-  author_email = 'dennis@irunasroot.com',
-  url = 'https://github.com/irunasroot/pynab',
-  download_url = 'https://github.com/irunasroot/pynab/archive/v0.1.tar.gz',
-  keywords = ['ynab', 'pynab', 'budgeting', "budgets"],
+  name="pynab-client",
+  packages=["pynabapi"],
+  version="0.1",
+  license="GNU GPLv3",
+  description="Python API to access YNAB v1 API",
+  author="Dennis Whitney",
+  author_email="dennis@irunasroot.com",
+  url="https://github.com/irunasroot/pynab-client",
+  download_url="https://github.com/irunasroot/pynab-client/archive/v0.1.tar.gz",
+  keywords=["ynab", "pynab", "budgeting", "budgets"],
   install_requires=[
-          'requests'
-      ],
-  classifiers=[
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU GPLv3',
-    'Programming Language :: Python :: 3.6',
+          "requests"
   ],
+  classifiers=[
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Programming Language :: Python :: 3.6",
+  ]
 )
